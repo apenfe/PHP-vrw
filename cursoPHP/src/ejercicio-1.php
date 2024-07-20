ba86e014f2eedadaf17d9105ec5ab4816d2192f0
apenfe <adrian1414@hotmail.es>
2024-07-20 09:25:26 +0200

    Ejercicio 1 PHP

---
+++ cursoPHP/src/ejercicio-1.php
@@ -0,0 +1,16 @@
+<h2>Ejercicio 1</h2>
+<?php
+
+    /*
+     * crea dos variables cuyo nombre sea "uno" y "dos" e imprimelas por pantalla.
+     * Pon un comentario con el tipo de dato que contienen.
+     *
+     */
+
+    $uno = 1; // contiene un entero
+    $dos = "dos"; // contiene un string
+
+    echo "UNO: ".$uno."<br>";
+    echo "DOS: ".$dos."<hr>";
+
+?>
