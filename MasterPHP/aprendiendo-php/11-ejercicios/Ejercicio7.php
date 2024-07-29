912f3295ed85007d2eb2b10cf7e0f2581192c7ea
apenfe <adrian1414@hotmail.es>
2024-07-29 18:59:34 +0200

    listo para comenzar funciones

--- MasterPHP/aprendiendo-php/11-ejercicios/Ejercicio7.php
+++ MasterPHP/aprendiendo-php/11-ejercicios/Ejercicio7.php
@@ -1,3 +1,35 @@
 <?php
+/*
+ *  muestre todos los numeros entre dos numero que llegan por url, solo impares
+ */
 
+if(isset($_GET['num1']) && isset($_GET['num2'])){
+
+    $num1 = $_GET["num1"];
+    $num2 = $_GET["num2"];
+
+    if($num1 < $num2){
+
+        for($i = $num1; $i <= $num2; $i++){
+
+            if($i % 2 != 0){
+                echo $i."<br>";
+            }
+
+        }
+
+    }else if($num1 > $num2){
+
+        for($i = $num2; $i <= $num1; $i++){
+            if($i % 2 != 0){
+                echo $i."<br>";
+            }
+        }
+    }else{
+        echo $num1."<br>";
+    }
+
+}else{
+    echo "inserte dos numeros por url";
+}
 ?>
