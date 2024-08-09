b6bc6397c4e96a1cb020f63b48be94bbd3db2b73
apenfe <adrian1414@hotmail.es>
2024-08-09 10:42:14 +0200

    insercion de usuarios en bbdd

--- cursoPHP/src/app/crear.php
+++ cursoPHP/src/app/crear.php
@@ -13,7 +13,17 @@ function showError($errors,$field) {
     return $alert;
 }
 
-$errors = null;
+function setValueField($errors, $field, $textarea = false) {
+    if(isset($errors) && count($errors)>=1 && isset($field)){
+        if($textarea){
+            echo htmlspecialchars($_POST[$field]);
+        }else{
+            echo "value='" . htmlspecialchars($_POST[$field]) . "'";
+        }
+    }
+}
+
+$errors = [];
 
 if(isset($_POST["submit"])){
 
@@ -65,41 +75,55 @@ if(isset($_POST["submit"])){
         $role_validate = false;
         $errors['img'] = 'La imagen no es valida';
     }
+
+    if(count($errors)<=1){
+        $sql = "insert into users values(null,'{$_POST["name"]}','{$_POST["surname"]}','{$_POST["bio"]}','{$_POST["email"]}','".sha1($_POST["password"])."','{$_POST["role"]}',null);";
+        $insert_user = mysqli_query($db, $sql);
+        $insert_user = true;
+    }else{
+        $insert_user = false;
+    }
 }
 ?>
 
 
 <h2>Crear usuarios</h2>
 
+<?php if(count($errors) <= 1 && isset($_POST['submit']) && $insert_user != false){ ?>
+    <div class="alert alert-success">
+        El usuario se ha enviado correctamente
+    </div>
+<?php } ?>
+
 <form action="crear.php" method="POST" enctype="multipart/form-data">
 
     <label for="name">
         Nombre:
-        <input type="text" name="name" class="form-control">
+        <input type="text" name="name" class="form-control" <?php setValueField($errors, 'name'); ?> />
         <?php echo showError($errors,"name"); ?>
     </label>
     <br>
     <label for="surname">
         Apellidos:
-        <input type="text" name="surname" class="form-control">
+        <input type="text" name="surname" class="form-control" <?php setValueField($errors, 'surname'); ?>>
         <?php echo showError($errors,"surname"); ?>
     </label>
     <br>
     <label for="bio">
         Biografia:
-        <textarea name="bio" class="form-control"></textarea>
+        <textarea name="bio" class="form-control" <?php setValueField($errors, 'bio',true); ?>></textarea>
         <?php echo showError($errors,"bio"); ?>
     </label>
     <br>
     <label for="email">
         Email:
-        <input type="email" name="email" class="form-control">
+        <input type="email" name="email" class="form-control" <?php setValueField($errors, 'email'); ?>>
         <?php echo showError($errors,"email"); ?>
     </label>
     <br>
     <label for="image">
         Imagen:
-        <input type="file" name="image" class="form-control">
+        <input type="file" name="image" class="form-control" >
     </label>
     <br>
     <label for="password">
