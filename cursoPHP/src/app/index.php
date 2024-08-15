c7c805b758a14c7929ffbb26c4751f8ac4bca24d
apenfe <adrian1414@hotmail.es>
2024-08-15 09:10:57 +0200

    paginacion

--- cursoPHP/src/app/index.php
+++ cursoPHP/src/app/index.php
@@ -3,6 +3,31 @@ include 'includes/redirect.php';
 include 'includes/header.php'; // si pongo require_once permite cargarlo solo una vez
 
 $users = mysqli_query($db, "SELECT * FROM users;");
+$num_total_users = mysqli_num_rows($users);
+
+if($num_total_users > 0){
+    $rows_per_page = 3;
+    $page = false;
+
+    if(isset($_GET["page"])){
+        $page = $_GET["page"];
+    }
+
+    if(!$page){
+        $start = 0;
+        $page = 1;
+    }else{
+        $start = ($page - 1) * $rows_per_page;
+    }
+
+    $total_pages = ceil($num_total_users / $rows_per_page);
+
+    $sql = "SELECT * FROM users ORDER BY usuario_id DESC LIMIT {$start}, {$rows_per_page};";
+    $users = mysqli_query($db, $sql);
+
+}else{
+    echo "No hay usuarios registrados";
+}
 
 ?>
 
@@ -34,6 +59,36 @@ $users = mysqli_query($db, "SELECT * FROM users;");
     <?php } ?>
 
 </table>
+
+<?php if($num_total_users >=1 ) { ?>
+
+    <ul class="pagination">
+
+        <?php if($page == 1){ ?>
+            <li class="page-item disabled"><a href="?page=<?= $page-1 ?>" class="page-link"><<</a></li>
+        <?php }else{ ?>
+            <li class="page-item"><a href="?page=<?= $page-1 ?>" class="page-link"><<</a></li>
+        <?php } ?>
+
+        <?php for($i = 1; $i <= $total_pages; $i++){ ?>
+
+            <?php if($page == $i){ ?>
+                <li class="page-item disabled"><a href="#" class="page-link"><?= $i ?></a></li>
+            <?php }else{ ?>
+                <li class="page-item"><a href="?page=<?= $i ?>" class="page-link"><?= $i ?></a></li>
+            <?php } ?>
+
+        <?php } ?>
+
+        <?php if($page == $total_pages){ ?>
+            <li class="page-item disabled"><a href="?page=<?= $page+1 ?>" class="page-link">>></a></li>
+        <?php }else{ ?>
+            <li class="page-item"><a href="?page=<?= $page+1 ?>" class="page-link">>></a></li>
+        <?php } ?>
+
+    </ul>
+
+<?php } ?>
 
 <?php
 include 'includes/footer.php';
