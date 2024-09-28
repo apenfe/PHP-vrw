d4064a6f7180ab6e0e6790d72b985adfd9bc0756
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-09-28 19:12:33 +0200

    final poo

--- MasterPHP/aprendiendo-php-poo/12-autoload/index.php
+++ MasterPHP/aprendiendo-php-poo/12-autoload/index.php
@@ -15,6 +15,17 @@ class Principal {
         $this->categoria = new Categoria();
         $this->entrada = new Entrada();
     }
+
+    public function getUsuario() {
+        return $this->usuario;
+    }
+
+    public function informacion() {
+        echo __CLASS__;
+        echo "<br>";
+        echo __DIR__;
+    }
+
 }
 
 $usuario = new UsuarioAdmin();
@@ -27,3 +38,24 @@ echo "<br>";
 var_dump($principal->categoria);
 echo "<br>";
 var_dump($principal->entrada);
+
+echo "<br>";
+var_dump(get_class_methods($principal)); // devuelve los métodos de la clase
+
+// comprobar si existe una clase
+$clase = @class_exists('MisClases\Usuario');
+if ($clase) {
+    echo "<h1>La clase si existe</h1>";
+} else {
+    echo "<h1>La clase no existe</h1>";
+}
+echo "<br>";
+
+$clase = @class_exists('PanelAdministrador\Usuarios');
+if ($clase) {
+    echo "<h1>La clase si existe</h1>";
+} else {
+    echo "<h1>La clase no existe</h1>";
+}
+
+$principal->informacion();
